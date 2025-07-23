58caf494f3338b18d8906478494b4c2527a8f091
edwardLee <ghfy1245.dev@gmail.com>
2025-07-23 13:04:47 +0900

    init project

--- backend/spots/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Spot
-
-# code here.
-class SpotSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Spot
-        fields = '__all__'
--- backend/spots/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.spots, name='spots'),
-    path('<int:spot_id>/', views.spot_detail, name='spot-detail'),
-]

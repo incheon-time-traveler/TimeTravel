949564962f74683dd3235bce460fc523dc6f302b
edwardLee <ghfy1245.dev@gmail.com>
2025-08-31 17:23:50 +0900

    chore : import 뺴먹음

--- backend/spots/views.py
+++ backend/spots/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 import random
 # Create your views here.
 @api_view(['GET'])
